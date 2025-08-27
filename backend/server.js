7233ab242731cf6065cf2c60022cef99f0ca0b0e
mohitrajora <mohitrajora1432@gmail.com>
2025-08-27 23:17:02 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "*", // allow all origins OR replace * with your frontend URL
+    origin: ["http://localhost:5174", "https://anandconnect.onrender.com"], // allowed origins
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 
